746f0a0bde19c2289944253681707c716c79438c
Piot333 <isaacterranova97@gmail.com>
2025-04-09 17:05:27 +0200

    Add files via upload

---
+++ Nome_band.py
@@ -0,0 +1,10 @@
+# Nomi citta e animale
+
+citta = input("Insersci il nome della tua citta di origine: ")
+animale = input("Inserisci il nome del tuo animale domestico: ")
+
+#Genrazione nome Band
+nome_band = citta + " " + animale
+
+#output del nome essere
+print("il nome della tua band potrebbe essere:" , nome_band)
